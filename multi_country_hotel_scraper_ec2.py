8f29b2769101467565dd27e5b41e11caf7cf758f
esulzickis <esulzickis@toogoodtogo.com>
2025-11-10 20:04:49 +0100

    Test

--- multi_country_hotel_scraper_ec2.py
+++ multi_country_hotel_scraper_ec2.py
@@ -50,8 +50,8 @@ def insert_hotel_data_to_dynamodb(all_hotel_data):
         for data in all_hotel_data:
             table.put_item(
                 Item={
-                    'pk': data.get('country'),
-                    'sk': data.get('scraped_at')
+                    'country_hotel': data.get('country'),
+                    'scraped_at': data.get('scraped_at')
                 })
 
     except Exception as e:
